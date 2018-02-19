cc45771f4d0c0c537d5b6ee803341f62795d3c95
r kip <gjr.kip@gmail.com>
2018-02-19 22:30:39 +0100

    not working but start on raytracer assignment

--- Karouch_Kip_Raytracter_1/raytracerframework_cpp(3)/Code/shapes/sphere.cpp
+++ Karouch_Kip_Raytracter_1/raytracerframework_cpp(3)/Code/shapes/sphere.cpp
@@ -22,13 +22,26 @@ Hit Sphere::intersect(Ray const &ray)
     * intersection point from the ray origin in *t (see example).
     ****************************************************/
 
-    // place holder for actual intersection calculation
+    //intersection calculation, used https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-sphere-intersection for helping with intersection calculation, TODO: is fine right?
+
+    //TODO: not working but fun result
 
     Vector OC = (position - ray.O).normalized();
-    if (OC.dot(ray.D) < 0.999) {
-        return Hit::NO_HIT();
-    }
-    double t = 1000;
+    double DOnOC = OC.dot(ray.D);
+    if(DOnOC<0) return Hit::NO_HIT(); //there is a hit behind the camera/eye so no hit //TODO: what if we want to implement reflection? Ray continues to bounce behind eye and back?
+    double d = sqrt(OC.dot(OC) - DOnOC*DOnOC);
+    if(d>r) return Hit::NO_HIT(); //distance between direction and circle origin is larger then radiance, so no hit
+    double lenHitC = sqrt(r*r - d*d); //length first hitpoint to sphere origin
+    double lenHitO1 = DOnOC - lenHitC; //lenght of first hit //TODO: do we want to return both intersection points?? or only that of the closest?
+    double lenHitO2 = DOnOC - lenHitC; //lenght of second hit. //TODO: do we ever need this?
+    double t = lenHitO1;
+
+    //original code, remove later TODO:
+//    Vector OC = (position - ray.O).normalized();
+//    if (OC.dot(ray.D) < 0.999) {
+//        return Hit::NO_HIT();
+//    }
+//    double t = 1000;
 
     /****************************************************
     * RT1.2: NORMAL CALCULATION
