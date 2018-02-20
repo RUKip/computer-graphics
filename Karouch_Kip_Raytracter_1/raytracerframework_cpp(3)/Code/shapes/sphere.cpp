0a75730412a09b53bdf8211ba2e30708dc271700
G.R. Kip <s2756781@fwn-bborg-edu-74-137.fmnsedu.rug.nl>
2018-02-20 16:49:05 +0100

    product of 2 hours of trying to find, normalise

--- Karouch_Kip_Raytracter_1/raytracerframework_cpp(3)/Code/shapes/sphere.cpp
+++ Karouch_Kip_Raytracter_1/raytracerframework_cpp(3)/Code/shapes/sphere.cpp
@@ -1,6 +1,7 @@
 #include "sphere.h"
 
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
@@ -22,26 +23,16 @@ Hit Sphere::intersect(Ray const &ray)
     * intersection point from the ray origin in *t (see example).
     ****************************************************/
 
-    //intersection calculation, used https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-sphere-intersection for helping with intersection calculation, TODO: is fine right?
-
-    //TODO: not working but fun result
-
-    Vector OC = (position - ray.O).normalized();
-    double DOnOC = OC.dot(ray.D);
-    if(DOnOC<0) return Hit::NO_HIT(); //there is a hit behind the camera/eye so no hit //TODO: what if we want to implement reflection? Ray continues to bounce behind eye and back?
-    double d = sqrt(OC.dot(OC) - DOnOC*DOnOC);
-    if(d>r) return Hit::NO_HIT(); //distance between direction and circle origin is larger then radiance, so no hit
-    double lenHitC = sqrt(r*r - d*d); //length first hitpoint to sphere origin
-    double lenHitO1 = DOnOC - lenHitC; //lenght of first hit //TODO: do we want to return both intersection points?? or only that of the closest?
-    double lenHitO2 = DOnOC - lenHitC; //lenght of second hit. //TODO: do we ever need this?
-    double t = lenHitO1;
-
-    //original code, remove later TODO:
-//    Vector OC = (position - ray.O).normalized();
-//    if (OC.dot(ray.D) < 0.999) {
-//        return Hit::NO_HIT();
-//    }
-//    double t = 1000;
+    //intersection calculation
+    Vector L  = (ray.O - position);
+    double b = 2*(ray.D.dot(L));
+    double c = L.dot(L) - r*r;
+    double discriminant = pow(b,2) - 4*c;
+    if(discriminant<0) return Hit::NO_HIT();
+    double hit1 = (-b + sqrt(discriminant))/2;
+    double hit2 = (-b - sqrt(discriminant))/2;
+    double t = min(hit1, hit2);
+    if(t<0) return Hit::NO_HIT();
 
     /****************************************************
     * RT1.2: NORMAL CALCULATION
@@ -52,7 +43,8 @@ Hit Sphere::intersect(Ray const &ray)
     * Insert calculation of the sphere's normal at the intersection point.
     ****************************************************/
 
-    Vector N /* = ... */;
+    Vector hitPoint = t*ray.D;
+    Vector N = hitPoint*position;
 
     return Hit(t,N);
 }
