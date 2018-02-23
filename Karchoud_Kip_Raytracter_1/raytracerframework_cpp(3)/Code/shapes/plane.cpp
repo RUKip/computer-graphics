cf6f89a61a4c2479f954fd432edc2d40cc9674a7
jamilkar <jamilkarchoud@hotmail.com>
2018-02-23 15:45:09 +0100

    plane almost working

--- Karchoud_Kip_Raytracter_1/raytracerframework_cpp(3)/Code/shapes/plane.cpp
+++ Karchoud_Kip_Raytracter_1/raytracerframework_cpp(3)/Code/shapes/plane.cpp
@@ -10,13 +10,21 @@ Hit Plane::intersect(Ray const &ray)
     //https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-plane-and-ray-disk-intersection
     //infinite plane
 
-    Vector L = (ray.D - pos);
+    //Direction
+    Vector L = (ray.O - pos);
 
-     if (L.dot(normal) > 1e-6) {
-        double t = ((pos - ray.O).dot(normal)) / L.dot(normal);
-        return Hit(t, normal);
+    //normalized normal
+    Vector N = normal.normalized();
+    double denum = L.dot(N);
+    double t = ((pos - ray.O).dot(N)) / denum;
+     if (denum > 0.000001) {
+
+        return Hit(t, N);
      }
-    return Hit::NO_HIT();
+
+
+     if (t < 0) return Hit::NO_HIT();
+
 }
 
 Plane::Plane(Point const &pos, Vector normal)
