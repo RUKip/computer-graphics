341962c929b3742b4277b47493910566c9a0df02
jamilkar <jamilkarchoud@hotmail.com>
2018-02-27 16:15:02 +0100

    same version work v2

--- Karchoud_Kip_OpenGL_2/OpenGL_2/Code/shaders/fragshader_phong.glsl
+++ Karchoud_Kip_OpenGL_2/OpenGL_2/Code/shaders/fragshader_phong.glsl
@@ -13,10 +13,13 @@ in vec3 vertNormal;
 
 // Specify the output of the fragment shader
 // Usually a vec4 describing a color (Red, Green, Blue, Alpha/Transparency)
-out vec3 fNormal;
+out vec4 fNormal;
 
 void main()
 {
-    fNormal = normalize(vertNormal);
-    fNormal /=3;
+    vec3 normal = normalize(vertNormal);
+    normal += vec3(1,1,1);
+    normal /= 3;
+    fNormal = vec4(normal, 1.0);
+
 }
