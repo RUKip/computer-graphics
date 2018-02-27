1d54b08b7385cd5ccdcd723167ec50f1e4fdaf88
r kip <gjr.kip@gmail.com>
2018-02-27 22:23:17 +0100

    added 1 comment

--- Karchoud_Kip_OpenGL_2/OpenGL_2/Code/shaders/vertshader_gouraud.glsl
+++ Karchoud_Kip_OpenGL_2/OpenGL_2/Code/shaders/vertshader_gouraud.glsl
@@ -7,6 +7,7 @@
 layout (location = 0) in vec3 vertCoordinates_in;
 layout (location = 1) in vec3 vertNormal_in;
 
+//TODO: why use uniform over input locations or otherway around for things like material.diffuse
 
 // Specify the Uniforms of the vertex shader
 // uniform mat4 modelTransform; for example
