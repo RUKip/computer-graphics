11b047259d29afe2c8b1b39cd08271835fc8ed31
G.R. Kip <s2756781@fwn-bborg-edu-74-49.fmnsedu.rug.nl>
2018-02-28 15:48:18 +0100

    shader implemented now to upload uniforms

--- Karchoud_Kip_OpenGL_2/OpenGL_2/Code/shaders/vertshader_gouraud.glsl
+++ Karchoud_Kip_OpenGL_2/OpenGL_2/Code/shaders/vertshader_gouraud.glsl
@@ -7,14 +7,15 @@
 layout (location = 0) in vec3 vertCoordinates_in;
 layout (location = 1) in vec3 vertNormal_in;
 
-//TODO: why use uniform over input locations or otherway around for things like material.diffuse
+//TODO: change material to input, input is per object
 
 // Specify the Uniforms of the vertex shader
 // uniform mat4 modelTransform; for example
 uniform mat4 projectionTransform_Gouraud;
 uniform mat4 modelTransform_Gouraud;
 uniform mat3 normalTransform_Gouraud;
-uniform vec3 material_Gouraud;
+uniform vec3 material_Color_Gouraud;
+uniform vec4 material_Components_Gouraud;
 uniform vec3 positionLight_Gouraud;
 uniform vec3 colorLight_Gouraud;
 
@@ -28,15 +29,13 @@ void main()
     // gl_Position is the output (a vec4) of the vertex shader
     gl_Position = projectionTransform_Gouraud * modelTransform_Gouraud * vec4(vertCoordinates_in, 1.0);
     vec3 vertNormal = normalTransform_Gouraud*vertNormal_in;
-    vec3 ambient = material_ambient_Gouraud*material_color_Gouraud;
-
+    vec3 ambient = material_Components_Gouraud.x*material_Color_Gouraud;
     vec3 diffuse = vec3(0.0,0.0,0.0);
     vec3 specular = vec3(0.0,0.0,0.0);
     //below should be done over all the lights,  //TODO: what if we want to incorparate more lights?? Do we send an array of lights to the shader??
-    double iDiffuse = max(0.0, dot(vertNormal, normalize((positionLight_Gouraud - vertCoordinates_in))));
-    double iSpecular = pow(max(0.0, dot(normalize(-reflect(L, normalize(vertNormal))), V)), specularMaterial); //TODO: what is our view V?
-    diffuse += material_diffuse_Gouraud*iDiffuse*material_color;
-    specular += material_specular_Gourad*iSpecular;
-
+    vec3 L = normalize(positionLight_Gouraud - gl_Position.xyz);
+    vec3 R = normalize(-reflect(L, normalize(vertNormal)));
+    vec3 diffuse = max(0.0, dot(L,vertNormal))*material_Color_Gouraud*material_Components_Gouraud.y*colorLight_Gouraud;
+    vec3 specular = pow(max(0.0, dot(R, gl_Position.xyz)), material_Components_Gouraud.w)*colorLight_Gouraud*material_Components_Gouraud.z;
     color = vec4(ambient+diffuse+specular,1.0);
 }
